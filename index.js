1ddb88a12f8d9aaba2fe34ec804146744827da5a
sfguide <support@SarasotaFLGuide.com>
2025-08-12 09:18:39 -0400

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get('/events', (req, res) => {
   const userStartDate = req.query.startDate ? new Date(req.query.startDate) : new Date();
   const classification = req.query.classificationName?.toLowerCase();
 
-  fs.createReadStream('Events_3.csv')
+  fs.createReadStream('Events_1.csv')
     .pipe(csv())
     .on('data', (row) => {
       const eventStart = new Date(row.startDate);
